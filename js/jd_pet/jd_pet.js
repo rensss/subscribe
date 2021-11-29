9a146a00b53bb1369b8af436ae3fc943f94f945c
Rzk <renzzz@qq.com>
2021-11-29 17:20:10 +0800

    fix bug

--- js/jd_pet/jd_pet.js
+++ js/jd_pet/jd_pet.js
@@ -32,7 +32,7 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify, new
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
   // 'MTAxODcxOTI2NTAwMDAwMDAyNTE5ODU4OQ==@MTAxODEyMjkyMDAwMDAwMDM5MzI3ODM1@MTE1NDUyMjEwMDAwMDAwMzUyNDI3Njk=@MTE1NDUyMjEwMDAwMDAwMzgxMjgwNjM=@MTE1NDAxNzYwMDAwMDAwMzk2NjQ2MjE=@MTE1NDQ5MzYwMDAwMDAwMzgwNzQxMTc=@MTAxODc2NTE0NzAwMDAwMDAyMTgwNDcwNw==',
-  'MTAxODc2NTEzMTAwMDAwMDAwNjI1Nzc5NQ=='
+  'MTAxODc2NTEzMTAwMDAwMDAwNjI1Nzc5NQ==',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
   // 'MTAxODcxOTI2NTAwMDAwMDAyNTE5ODU4OQ==@MTAxODEyMjkyMDAwMDAwMDM5MzI3ODM1@MTE1NDUyMjEwMDAwMDAwMzUyNDI3Njk=@MTE1NDUyMjEwMDAwMDAwMzgxMjgwNjM=@MTE1NDAxNzYwMDAwMDAwMzk2NjQ2MjE=@MTE1NDQ5MzYwMDAwMDAwMzgwNzQxMTc=@MTAxODc2NTE0NzAwMDAwMDAyMTgwNDcwNw==',
   'MTAxODc2NTEzMTAwMDAwMDAwNjI1Nzc5NQ==',
